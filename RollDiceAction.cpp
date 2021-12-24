0c1e4f449ae2c62d048545b1976970eec10d59e2
Ibraam Nashaat <ibraam.nashaat2@gmail.com>
2021-12-24 13:38:57 +0200

    RollDiceAction

--- RollDiceAction.cpp
+++ RollDiceAction.cpp
@@ -5,7 +5,7 @@
 
 #include <time.h> // used to in srand to generate random numbers with different seed
 
-RollDiceAction::RollDiceAction(ApplicationManager *pApp) : Action(pApp)
+RollDiceAction::RollDiceAction(ApplicationManager* pApp) : Action(pApp)
 {
 }
 
@@ -16,26 +16,31 @@ void RollDiceAction::ReadActionParameters()
 
 void RollDiceAction::Execute()
 {
-		
+
 	///TODO: Implement this function as mentioned in the guideline steps (numbered below) below
 
 
 	// == Here are some guideline steps (numbered below) to implement this function ==
 
 	// 1- Check if the Game is ended (Use the GetEndGame() function of pGrid), if yes, make the appropriate action
-	
+	Grid* pGrid = pManager->GetGrid();
+	Output* pOut = pGrid->GetOutput();
+	bool gameEnded = pGrid->GetEndGame();
+
 	// -- If not ended, do the following --:
-
-	// 2- Generate a random number from 1 to 6 --> This step is done for you
-	srand((int)time(NULL)); // time is for different seed each run
-	int diceNumber = 1 + rand() % 6; // from 1 to 6 --> should change seed
-
-	// 3- Get the "current" player from pGrid
-
-	// 4- Move the currentPlayer using function Move of class player
-
-	// 5- Advance the current player number of pGrid
-
+	if (!gameEnded)
+	{
+		// 2- Generate a random number from 1 to 6 --> This step is done for you
+		srand((int)time(NULL)); // time is for different seed each run
+		int diceNumber = 1 + rand() % 6; // from 1 to 6 --> should change seed
+		pOut->PrintMessage("The dicenumber is " + to_string(diceNumber));
+		// 3- Get the "current" player from pGrid
+		Player* currentPlayer = pGrid->GetCurrentPlayer();
+		// 4- Move the currentPlayer using function Move of class player
+		currentPlayer->Move(pGrid, diceNumber);
+		// 5- Advance the current player number of pGrid
+		pGrid->AdvanceCurrentPlayer();
+	}
 
 	// NOTE: the above guidelines are the main ones but not a complete set (You may need to add more steps).
 
